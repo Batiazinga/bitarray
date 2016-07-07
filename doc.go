155dd868a8488e509ac4ef523a036feba6875def
Batiazinga <luc.nguyenthe@gmail.com>
2016-07-07 21:59:38 +0200

    Add a doc for the package

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package bitarray implements arrays with single bit booleans.
+
+Compared to an array of built-in booleans, this saves memory (about eight times cheaper)
+but reading and setting values is much slower (roughly ten times slower).
+*/
+package bitarray
